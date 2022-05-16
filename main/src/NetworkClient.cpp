34734acdbdfedbb6e870661bf0fb7100a6d293b3
egeemirozkanjf <egeozkan@jotform.com>
2022-05-16 15:13:08 +0300

    add html

--- main/src/NetworkClient.cpp
+++ main/src/NetworkClient.cpp
@@ -94,3 +94,59 @@ void VaultSignal::NetworkClient::sendEvents(void)
         }
     }
 }
+
+static const NetworkCredentials &initializeWifiProvisioning(const char *apName)
+{
+    WiFi.softAP(apName, 'Cat');
+    WiFiServer passwordServer(80);
+    WiFiClient client = server.available(); // Listen for incoming clients
+
+    // Ported from the tutorial at: https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
+
+    if (client)
+    { // If a new client connects,
+        while (client.connected())
+        { // loop while the client's connected
+            if (client.available())
+            {                           // if there's bytes to read from the client,
+                char c = client.read(); // read a byte, then
+                Serial.write(c);        // print it out the serial monitor
+                header += c;
+                if (c == '\n')
+                { // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0)
+                    {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // Display the HTML web page
+                        client.println(payload);
+                        client.println();
+                        // Break out of the while loop
+                        break;
+                    }
+                    else
+                    { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+                }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
+    }
+}
